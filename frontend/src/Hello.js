5c7774654655ccf038d6cc29b48087db30641d1b
bnziggy <brettziegler12@gmail.com>
2024-07-10 12:24:28 -0400

    Create frontend and backend

---
+++ frontend/src/Hello.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api/hello')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
